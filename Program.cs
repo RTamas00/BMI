53efd94eb60070931a811e97aa7268b7fd53f631
radvanyit <radvanyit@310-06>
2026-01-19 08:38:15 +0100

    proba commit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
-﻿namespace BMI
+﻿using System.Diagnostics.Tracing;
+
+namespace BMI
 {
-    public class Program
+    internal class Program
     {
-      static void Main(string[] args)
+        static void Main(string[] args)
         {
-            List<Diak> diak = new List<Diak>()
+            List<Diak> list = new List<Diak>();
+            string[] sorok = File.ReadAllLines("BMI.txt"
+                , System.Text.Encoding.Latin1);
+            foreach (var sor in sorok)
             {
-               
-            };
+                Console.WriteLine(sor);
+                string[] darabok = sor.Split(';');
+                string név = darabok[0];
+                int életkor = Convert.ToInt32(darabok[1]);
+                int magasság = Convert.ToInt32(darabok[2]);
+                int súly = Convert.ToInt32(darabok[3]);
+                Diak d = new Diak(név, életkor, magasság, súly);
+                List.Add(d);
+            }
         }
+        }
+
     }
-}
+
